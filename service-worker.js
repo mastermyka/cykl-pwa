04295d4dc2977caa3a632eaa1350e0074d333feb
mastermyka <mastermyka@wp.pl>
2026-01-02 00:59:51 +0100

    Update cache name and improve service worker logic

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,33 @@
-const cacheName = 'cykl-app-cache-v2';
+const cacheName = 'cykl-pwa-v1';
 const filesToCache = [
-'/',
-'/index.html',
-'/manifest.json',
-'/icon-192.png',
-'/icon-512.png'
+  'index.html',
+  'manifest.json',
+  'icon-512.png'
 ];
 
-
+// Instalacja
 self.addEventListener('install', event => {
-event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+  self.skipWaiting();
 });
 
+// Aktywacja
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => { if(key !== cacheName) return caches.delete(key); })
+      )
+    )
+  );
+  self.clients.claim();
+});
 
+// Obsługa fetch
 self.addEventListener('fetch', event => {
-event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
